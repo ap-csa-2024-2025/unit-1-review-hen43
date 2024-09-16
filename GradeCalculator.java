2df9ce4704d76b34087fca31a71843800074453f
hen43 <whshle08@gmail.com>
2024-09-16 10:54:23 -0700

    skibidi ohio on gyatt
    sigmamaxxing

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,25 +1,27 @@
 public class GradeCalculator
 {
-  static double listAverage(double[] list){
-    double var = 0;
-    for (double i : list){
-      var += i;
-    }
-    var /= list.length;
-    return var;
-  }
-
   public static void main(String[] args)
   {
     String courseName = "Fortnite 90 Cranking Bootcamp";
     int courseMinutesWeekly = 333;
-    int[] homeworkScores = new int[]{58, 80, 100, 67};
-    double[] quizScores = new double[]{70.8, 88.9};
-    double finalScore = 60.5;
+    int hw1 = 58;
+    int hw2 = 80;
+    int hw3 = 100;
+    int hw4 = 67;
+    double quiz1 = 70.8;
+    double quiz2 = 88.9;
+    double final1 = 60.5;
+    int avghw = (hw1 + hw2 + hw3 + hw4)/4;
+    double avgquiz = (quiz1 + quiz2)/2;
 
     System.out.println(courseName);
     System.out.println("Average time spent: " + (int)(courseMinutesWeekly/60) + " hours, " + courseMinutesWeekly % 60 + " minutes.");
-    System.out.println(listAverage(homeworkScores));
-    System.out.println(listAverage(quizScores));
+    System.out.println("Homework grades:");
+
+    System.out.println("Average HW Grade: " + avghw);
+    System.out.println("Average Quiz Grade: " + avgquiz);
+    System.out.println("Final Exam Score: " + final1);
+    
+    System.out.println("Final Grade: " + (int)((avghw * 0.35)+(avgquiz * 0.15)+(final1 * 0.50)+0.5));
   }
 }
